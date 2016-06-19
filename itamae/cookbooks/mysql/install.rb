570bde2016da99b52d83dea54507cffa43a9287c
unknown <>
2016-06-19 17:48:31 +0900

    Adjust regex convert MariaDB download url

--- itamae/cookbooks/mysql/install.rb
+++ itamae/cookbooks/mysql/install.rb
@@ -1,18 +1,31 @@
-dir_user_home = node[:dir_user_home]
-unless dir_user_home.end_with?("/") then
-    dir_user_home << '/'
+#!/usr/bin/ruby
+
+if node[:dir_user_home].nil? || node[:dir_user_home].empty?
+    dir_user_home = '/home/vagrant'
+else
+    dir_user_home = node[:dir_user_home]
+    unless dir_user_home.end_with?("/") then
+        dir_user_home << '/'
+    end
 end
 
-dir_tmp = node[:dir_tmp]
-if dir_tmp.end_with?("/") then
-    dir_tmp = dir_tmp.slice(0, dir_tmp.length - 1)
+if node[:dir_tmp].nil? || node[:dir_tmp].empty?
+    dir_tmp = '/tmp'
+else
+    dir_tmp = node[:dir_tmp]
+    if dir_tmp.end_with?("/") then
+        dir_tmp = dir_tmp.slice(0, dir_tmp.length - 1)
+    end
 end
 
-mysql_version = 'mariadb-10.1.13'
-if node.has_key?("optional") && node[:optional].has_key?("mysql_version") && node[:optional][mysql_version] then
-    mysql_version = node[:optional][mysql_version]
+if node[:mysql][:version].nil? || node[:mysql][:version].empty?
+    mysql_version = 'mariadb-10.1.13'
+else
+    mysql_version = node[:mysql][:version]
 end
 
+puts "mysql: dir_user_home: #{dir_user_home}"
+puts "mysql: dir_tmp: #{dir_tmp}"
 puts "mysql: install #{mysql_version}"
 
 %w(mysql-client build-essential dpkg-dev devscripts hardening-wrapper autoconf automake1.9 autotools-dev binutils bison chrpath debhelper doxygen dpatch dvipdfmx fakeroot fontconfig-config g++ gawk gcc gettext ghostscript ghostscript-x gsfonts html2text intltool-debian libc6-dev libcroco3 libcups2 libcupsimage2 libfontconfig1 libfontenc1 libfreetype6 libgomp1 libice6 libjpeg62 libltdl7 libltdl7-dev libmail-sendmail-perl libncurses5-dev libpaper-utils libpaper1 libpng12-0 libsm6 libsys-hostname-long-perl libtool libwrap0-dev libxaw7 libxfont1 libxmu6 libxpm4 libxt6 linux-libc-dev lmodern m4 make patchutils po-debconf tex-common texlive-base texlive-base-bin texlive-latex-base texlive-latex-base-doc ttf-dejavu ttf-dejavu-core ttf-dejavu-extra xfonts-encodings xfonts-utils zlib1g-dev).each do |pkg|
@@ -48,7 +61,7 @@ end
 
 execute 'mysqlenv settings' do
     command <<-"EOH"
-sed -i 's|http://archive\.maiadb\.org.\+#{mysql_version}\.tar\.gz|https://downloads.mariadb.org/interstitial/#{mysql_version}/source/#{mysql_version}.tar.gz|' #{dir_user_home}.mysqlenv/mysql-build/share/mysql-bulid/definitions/#{mysql_version}
+sed -i 's|http://archive\.mariadb\.org.\+#{mysql_version}\.tar\.gz|https://downloads.mariadb.org/interstitial/#{mysql_version}/source/#{mysql_version}.tar.gz|' #{dir_user_home}.mysqlenv/mysql-build/share/mysql-bulid/definitions/#{mysql_version}
 export PATH=#{dir_user_home}.mysqlenv:$PATH; eval "$(mysqlenv init -)"; mysqlenv install #{mysql_version}
 export PATH=#{dir_user_home}.mysqlenv:$PATH; eval "$(mysqlenv init -)"; mysqlenv global #{mysql_version}
 EOH
