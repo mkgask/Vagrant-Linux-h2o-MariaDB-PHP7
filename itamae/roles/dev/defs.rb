ce74c3f6c04932715735ea90ba4479becf7c19d7
unknown <>
2016-06-24 02:03:26 +0900

    Add platform auto detect function and install function

---
+++ itamae/roles/dev/defs.rb
@@ -0,0 +1,22 @@
+def setup_filenames(prefix, platform, platform_version, postfix)
+    return [
+        prefix + '-' + platform + '-' + platform_version + postfix,
+        prefix + '-' + platform + postfix,
+        prefix + postfix,
+    ]
+end
+
+def install(cookbook, files, cookbookdir)
+    if cookbook.nil? || cookbook.empty? || files.nil? || files.empty? || cookbookdir.nil? || cookbookdir.empty?
+        return ''
+    end
+    files.each do |filename|
+        search_file = __dir__ + '/' + cookbookdir + '/' + cookbook + '/' + filename
+        if File.exist? search_file
+            puts cookbook + ': Found recipe: ' + filename
+            include_recipe search_file
+        else
+            puts cookbook + ': Not found: ' + filename
+        end
+    end
+end
--- itamae/roles/dev/setup.rb
+++ itamae/roles/dev/setup.rb
@@ -1,20 +1,26 @@
 #!/usr/bin/ruby
 
+require __dir__ + '/defs.rb'
+
+port_settings_filenames  = setup_filenames('port-settings', node['platform'], node['platform_version'], '.rb')
+setup_filenames = setup_filenames('install', node['platform'], node['platform_version'], '.rb')
+#puts 'setup_filenames: ' << setup_filenames.to_s
+
 path_recipe_base = '../../cookbooks/'
 
 puts 'os: default setup'
-include_recipe path_recipe_base + 'os/port-settings-ubuntu_trusty.rb'
+install 'os', port_settings_filenames, path_recipe_base
 include_recipe path_recipe_base + 'os/set-timezone.rb'
 include_recipe path_recipe_base + 'os/update-upgrade.rb'
 
 if node[:h2o] then
-    include_recipe path_recipe_base + 'h2o/install-ubuntu_trusty.rb'
+    install 'h2o', setup_filenames, path_recipe_base
 end
 
 if node[:php] then
-    include_recipe path_recipe_base + 'php/install-ubuntu_trusty.rb'
+    install 'php', setup_filenames, path_recipe_base
 end
 
 if node[:mysql] then
-    include_recipe path_recipe_base + 'mysql/install.rb'
+    install 'mysql', setup_filenames, path_recipe_base
 end
